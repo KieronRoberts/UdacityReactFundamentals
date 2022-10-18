63a26525f49b9d199731833a88794e771ab3c1ba
Kieron Roberts <khr2000@hotmail.co.uk>
2022-10-18 16:45:45 +0100

    Post review changes

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# MyReads: A Book Tracking App
+
+The inital project for the Udacity React Fundamentals course. Allowing me to implement skills I have learnt over the last four weeks.
+
+
+## Acknowledgements
+
+ - [Inital Codes](https://github.com/udacity/nd0191-c1-myreads/)
+ - [W3Schools React Tutorial](https://www.w3schools.com/react/default.asp)
+ - [How to write a readme](https://readme.so/editor)
+
+
+## Documentation
+
+[Documentation](https://linktodocumentation)
+
+
+## Authors
+
+- [@KieronRoberts](https://github.com/KieronRoberts)
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/KieronRoberts/UdacityReactFundamentals
+```
+
+Go to the project directory
+
+```bash
+  cd UdacityReactFundamentals
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
--- myreads/src/components/Book.js
+++ myreads/src/components/Book.js
@@ -1,12 +1,12 @@
 import React from 'react';
 /*Defines Book for use acrross the application*/
 const Book = ({ book, changeBookShelf }) => {
-
     return (
         <div className="book">
             <div className="book-top">
                 {/*Defines where the image of the of the book will come from and how it should look*/}
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ book.imageLinks !== undefined ? book.imageLinks.smallThumbnail : ""}")`,}}>
+                </div>
                 {/*Creates the charger on each book*/}
                 <div className="book-shelf-changer">
                     <select defaultValue={book.shelf ? book.shelf : "none"} onChange={(e) => changeBookShelf(book, e.target.value)}>
@@ -20,7 +20,7 @@ const Book = ({ book, changeBookShelf }) => {
             </div>
             {/* Defines both the book title and the author*/}
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.publisher}</div>
+            <div className="book-authors">{book.authors}</div>
         </div>
     )
 }
